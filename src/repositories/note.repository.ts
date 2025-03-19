c17ed905a153944d8734bb2de821a08d6bd339d5
Rafli Dewanto <dewantorafli@gmail.com>
2025-03-19 22:49:28 +0700

    Day 1: Auth

--- src/repositories/note.repository.ts
+++ src/repositories/note.repository.ts
@@ -5,7 +5,58 @@ import { PaginationParams } from "../types/pagination";
 import { getErrorMessage } from "../utils/error";
 
 class NoteRepository {
-  
+  private prisma: PrismaClient;
+
+  constructor(prisma: PrismaClient) {
+    this.prisma = prisma;
+  }
+
+  async findAll(
+    userId: number,
+    pagination?: PaginationParams,
+    filters?: NoteFilters
+  ): Promise<{ notes: Note[]; total: number } | string> {
+    try {
+      const page = pagination?.page || 1;
+      const limit = pagination?.limit || 12;
+      const skip = (page - 1) * limit;
+
+      const where: Prisma.NoteWhereInput = {
+        isDeleted: false,
+        userId,
+        ...(filters?.search && {
+          OR: [
+            {
+              title: {
+                contains: filters.search,
+                mode: Prisma.QueryMode.insensitive,
+              },
+            },
+            {
+              content: {
+                contains: filters.search,
+                mode: Prisma.QueryMode.insensitive,
+              },
+            },
+          ],
+        }),
+        ...(filters?.startDate && { createdAt: { gte: filters.startDate } }),
+        ...(filters?.endDate && { createdAt: { lte: filters.endDate } }),
+      };
+
+      const [] = await Promise.all([
+        this.prisma.note.findMany({
+          where,
+          skip,
+          take: limit,
+          orderBy: {
+            createdAt: "desc",
+          },
+        }),
+        
+      ]);
+    } catch (error) {}
+  }
 }
 
 export default NoteRepository;
